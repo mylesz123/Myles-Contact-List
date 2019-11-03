d27ad19d8f1c04ef7a94107dab3fdab6d685a674
Myles Young <myles.young@mailchimp.com>
2019-11-03 13:19:24 -0500

    Added design and navigation for Birthdate change button

--- Myles Contact List/ContactsViewController.swift
+++ Myles Contact List/ContactsViewController.swift
@@ -9,12 +9,59 @@
 import UIKit
 
 class ContactsViewController: UIViewController {
-
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // When the scene is about to appear, a method is called to register the code to listen for notifications that the keyboard has been displayed.
+        self.registerKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.unregisterKeyboardNotifications()
+    }
+    
+    /*This method registers the code for notifications and tells the system to execute the appropriate method when the event occurs.*/
+    func registerKeyboardNotifications() {
+       NotificationCenter.default.addObserver(
+        self,
+        selector: #selector(self.keyboardDidShow(notification:)),
+        name: UIResponder.keyboardDidShowNotification,
+        object: nil
+        )
+    }
+    
+    func unregisterKeyboardNotifications() {
+       NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func keyboardDidShow(notification: NSNotification) {
+        let userInfo: NSDictionary = notification.userInfo! as NSDictionary
+        let keyboardInfo = userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue
+        let keyboardSize = keyboardInfo.cgRectValue.size
+        
+//        get existing ConsentTnset for the scrollView and set the bottom prop to be the height of the keyboard
+        var contentInset = self.scrollView.contentInset
+        contentInset.bottom = keyboardSize.height
+        
+        self.scrollView.contentInset = contentInset
+        self.scrollView.scrollIndicatorInsets = contentInset
+    }
+    
+    /*When the keyboard disappears, the scroll view’s content insert values are set back to the original values.*/
+    func keyboardWillHide(notification: NSNotification) {
+        var contentInset = self.scrollView.contentInset
+        contentInset.bottom = 0
+        
+        self.scrollView.contentInset = contentInset
+        self.scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+    }
 
 }
 
