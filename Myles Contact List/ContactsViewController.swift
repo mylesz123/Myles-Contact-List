f8df68804786f7a467f28fa12a9e78fc05289c29
Myles Young <myles.young@mailchimp.com>
2019-11-03 09:08:27 -0500

    Made layout for map, settings, and contacts screens. Added view controllers for each layout.

--- Myles Contact List/ContactsViewController.swift
+++ Myles Contact List/ContactsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class ContactsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Myles Contact List/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  Myles Contact List
+//
+//  Created by Myles Young on 11/2/19.
+//  Copyright © 2019 Myles Young. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
