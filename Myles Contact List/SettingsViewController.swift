11a41a54339682db61e93193fe5a699f469ea0a0
Myles Young <myles.young@mailchimp.com>
2019-11-04 08:08:02 -0500

    Updated settings controller with methods to show items in the picker when viewing the settings scene.

--- Myles Contact List/SettingsViewController.swift
+++ Myles Contact List/SettingsViewController.swift
@@ -8,15 +8,47 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-
+class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var switchAsc: UISwitch!
+    @IBOutlet weak var pickSortField: UIPickerView!
+    
+    let sortOrderItems: Array<String> = [
+        "ContactName", "City", "Birthday"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        pickSortField.dataSource = self
+        pickSortField.delegate = self
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+//    UIPicker Delegates and data source methods Data Sources
+//    return # of rows to display
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+//    returns number of rows in picker
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return sortOrderItems.count
+    }
 
+//    set value that is displayed in each row in the picker
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return sortOrderItems[row]
+    }
+    
+//    if the user selects from the pickerView...
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("Chosen Item: \(sortOrderItems[row])")
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -26,5 +58,6 @@ class SettingsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func sortDirectionChanged(_ sender: Any) {
+    }
 }
