5185fe2e90663a32368c4e17c1de9b5388843cb9
Myles Young <myles.young@mailchimp.com>
2019-11-04 08:13:18 -0500

    pick back up in the book @Working with the UserDefaults Object

--- Myles Contact List/SettingsViewController.swift
+++ Myles Contact List/SettingsViewController.swift
@@ -29,11 +29,13 @@ class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPicker
         super.didReceiveMemoryWarning()
     }
     
-//    UIPicker Delegates and data source methods Data Sources
+/*    UIPicker Delegates and data source methods Data Sources*/
+    
 //    return # of rows to display
     public func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
+    
 //    returns number of rows in picker
     public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return sortOrderItems.count
@@ -44,20 +46,11 @@ class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPicker
         return sortOrderItems[row]
     }
     
-//    if the user selects from the pickerView...
+//    print the item that was selected
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         print("Chosen Item: \(sortOrderItems[row])")
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     @IBAction func sortDirectionChanged(_ sender: Any) {
     }
 }
