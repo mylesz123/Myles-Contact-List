5bb8de53a705079daa06ffe06b147a92f1da48b6
cohaya1 <chika.ohaya@gmail.com>
2019-11-18 14:52:06 -0500

    Part of maps completed

--- Myles Contact List/MapViewController.swift
+++ Myles Contact List/MapViewController.swift
@@ -5,16 +5,32 @@
 //  Created by Myles Young on 11/2/19.
 //  Copyright © 2019 Myles Young. All rights reserved.
 //
-
+import MapKit
 import UIKit
-
+import CoreLocation
 class MapViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+
+        locationManager.delegate = self as? CLLocationManagerDelegate
+        mapView.delegate = self as? MKMapViewDelegate
+        locationManager.requestWhenInUseAuthorization()    }
+
+  //   override func viewWillAppear(_ animated: Bool) {
+        //mapView.setUserTrackingMode(.follow, animated: true)
+
+
+        
+
+//}
+
+    @IBAction func findUser(_ sender: Any) {
+        mapView.showsUserLocation = true
+        mapView.setUserTrackingMode(.follow, animated: true)
     }
-
-
 }
-
